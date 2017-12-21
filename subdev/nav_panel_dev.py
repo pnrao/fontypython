5dada490197ecc9a35e47f2b2a905208572b22d0
donningle <donningle@10e3e7b3-dc37-4a6a-98fe-1e456c7b9c7b>
2017-12-21 10:51:10 +0000

    ok

--- subdev/nav_panel_dev.py
+++ subdev/nav_panel_dev.py
@@ -27,23 +27,35 @@ class NavPanel(wx.PyPanel):
         self.SetBackgroundColour('yellow')
         #self.st = wx.StaticText(self, -1, "test", size = sz )
 
-        self._items = []
-        self._current_page = self._last_page = 0
+        self._items = {}
+        self._current_page = self._last_page = 1
 
-        self.hz = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.iid_prev = wx.NewId()
-        self.iid_next = wx.NewId()
-            
 
-        self.SetSizer(self.hz)
-
-        self.Bind(wx.EVT_SIZE, self.OnSize)    
         
         f = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
         self.ps = 14
         f.SetPointSize(self.ps)
         self.tile_size = f.GetPixelSize()
+
+        self.hz = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.iid_prev = wx.NewId()
+        self.iid_next = wx.NewId()
+
+        self.gst_prev = self._text(self.iid_prev,"<")
+        self.gst_next = self._text(self.iid_next,">")
+
+        self.hz.Add(self.gst_prev)
+        self.hz.AddSpacer(10)
+        self.hz.Add(self.gst_next)            
+
+        self.SetSizer(self.hz)
+        self.Layout()
+
+
+        self.Bind(wx.EVT_SIZE, self.OnSize)    
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
 
     def _chooser(self):
         ## The pager pulldown
@@ -96,90 +108,112 @@ class NavPanel(wx.PyPanel):
         f.SetWeight(wx.FONTWEIGHT_NORMAL)
         gst.SetFont(f)
 
-    def set_list(self, numrange = None):
-        if not numrange: 
-            numrange = self._numrange
+    def OnPaint(self,evt):
+        # seems to happen on the various Set... calls
+        pass
+
+    def set_list(self, n):#numrange = None):
+        if not n: 
+            print "set_list no n"
+            n = self._n
             to_pg = self._current_page
             #self._current_page = self._last_page = 0
         else:
+            print "set_list with n"
             #self._reset()
-            self._current_page = self._last_page = 0
+            self._current_page = self._last_page = 1
             to_pg = 1
 
-        #self.hz.Clear(True)
-        self._numrange = numrange
-        #self._items[:] #keeps list defined        
+        self._n = n
 
-        #pansz = self.GetSize()
-        #panw = pansz[0]
-        #print "panw:",panw
+        self.hz.Remove(1)
 
-        #L = len(numrange)
-        #listw = L * (self.tile_size[0]+4) + (2*(self.tile_size[0]+4))
-        #print "listw:", listw
+        #fgz = self._draw_the_numbers(numrange)
+        fgz = self.abbr_pages(n,to_pg)
 
-        if False: #listw > panw:
-            #how much of list can fit in panw?
-            d = listw - panw
-            overtiles = d / self.tile_size[0]
-            oktiles = panw / self.tile_size[0]
-            print overtiles
-            print oktiles
-            print
+        self.hz.Insert(1,fgz,0)
 
-
-        fgz = self._draw_the_numbers(numrange)
-         
-        #self.gst_prev = GenStaticText(self,self.iid_prev,"<")
-        self.gst_prev = self._text(self.iid_prev,"<")
-        self.gst_next = self._text(self.iid_next,">")
-
-        self.hz.Add(self.gst_prev)
-        self.hz.Add(fgz)
-        self.hz.Add(self.gst_next)
         self.Layout()
         print "size of hz:", self.hz.GetSize()
         self.select_page_number(to_pg)
 
-    def _draw_the_numbers(self,numrange):#start,end):
-        self.hz.Clear(True)
-        del self._items[:] #keeps list defined        
+
+
+    def abbr_pages(self, n, page):
+        """
+        Return a string containing the list of numbers from 1 to `n`, with
+        `page` indicated, and abbreviated with ellipses if too long.
+
+        >>> abbreviated_pages(5, 3)
+        '1 2 [3] 4 5'
+        >>> abbreviated_pages(10, 10)
+        '1 2 3 4 5 6 7 8 9 [10]'
+        >>> abbreviated_pages(20, 1)
+        '[1] 2 3 ... 18 19 20'
+        >>> abbreviated_pages(80, 30)
+        '1 2 3 ... 28 29 [30] 31 32 ... 78 79 80'
+        """
+        assert(0 < n)
+        assert(0 < page <= n)
+
+        # Build set of pages to display
+        if n <= 10:
+            pages = set(range(1, n + 1))
+        else:
+            pages = (set(range(1, 4))
+                     | set(range(max(1, page - 2), min(page + 3, n + 1)))
+                     | set(range(n - 2, n + 1)))
+
+        # Display pages in order with ellipses
+        def display():
+            last_page = 0
+            for p in sorted(pages):
+                if p != last_page + 1: yield '...'
+                #yield ('[{0}]' if p == page else '{0}').format(p)
+                yield '{0}'.format(p)
+                last_page = p
+        
+        pgs=[]
+        for pg in display(): 
+            pgs.append(pg)
+
+        for pgnum, gst in self._items.iteritems():
+            gst.Unbind(wx.EVT_MOUSE_EVENTS)
+            gst.Destroy()
+        self._items.clear()
+
         fgs = wx.FlexGridSizer(cols = 1, vgap = 0, hgap = 4)
-        cols = len(numrange)
+        cols = len(pgs)
         fgs.SetCols(cols)
-        #self._items.append('dud')
-        for i in numrange:
-            iid = wx.NewId()
-            lbl = unicode(i)
-            #gst = GenStaticText(self, iid, lbl)
-            gst = self._text(-1, lbl)
-            d = {'iid':iid,'gst':gst}
-            self._items.append(d)
 
-            gst.Bind(wx.EVT_MOUSE_EVENTS, self.OnMouseEvent)
-            #gst.Bind(wx.EVT_MOTION, self.OnMouseEvent)
-
+        for pg in pgs:
+            lbl = unicode(pg)
+            gst = self._text(-1,lbl)
+            if lbl != "...":
+                self._items.update({int(lbl):gst})
+                gst.Bind(wx.EVT_MOUSE_EVENTS, self._pagelink_mouse_event)
             fgs.Add(gst,0,wx.EXPAND)
         return fgs
-    
+            
     def _focus_off(self, item_index):
-        gst = self._items[item_index-1]['gst']
+        gst = self._items.get(item_index,None)
         gst.SetBackgroundColour('yellow')
         self._unbold(gst)
 
     def _focus_on(self, item_index):
-        gst = self._items[item_index-1]['gst']
+        gst = self._items.get(item_index,None)
         gst.SetBackgroundColour('red')
         self._enbold(gst)
 
     def select_page_number(self, item_index):
+        print self._current_page
         self._focus_off(self._current_page)
         self._last_page = self._current_page
         self._current_page = item_index
         self._focus_on(item_index)
 
 
-    def OnMouseEvent(self, e):
+    def _pagelink_mouse_event(self, e):
 
         #print dir(e)
         #print e.GetEventObject().GetLabel()
@@ -190,20 +224,20 @@ class NavPanel(wx.PyPanel):
         elif e.LeftUp():
             obj = e.GetEventObject()
             page_index = int(obj.GetLabel())
-            iid = obj.GetId()
+            #iid = obj.GetId()
             #print "goes to:", page
             self.select_page_number( page_index )
-            wx.CallAfter(self._page_clicked, page_index) # execs in the main thread after this event is done.
+            wx.CallAfter(self._pagelink_clicked, page_index) # execs in the main thread after this event is done.
+            #wx.CallAfter(self.set_list) # go back and re-draw myself.
         else:
             self.SetCursor(wx.NullCursor)
         e.Skip()
 
-    def _page_clicked(self, page_index):
+    def _pagelink_clicked(self, page_index):
         print "call after for:", page_index
         evt = PagedEvent(myEVT_PAGED, self.GetId()) 
         evt.page = page_index
         self.GetEventHandler().ProcessEvent(evt)
-        self.set_list()
 
 #for kid in self.fitmap_sizer.GetChildren():
 #    fitmap = kid.GetWindow() #is the fitmap within
@@ -243,7 +277,7 @@ class Example(wx.Frame):
         self.np = pan_bot
         vz.Add(pan_bot,0,wx.EXPAND)
 
-        self.np.set_list(xrange(1,21))
+        self.np.set_list(20)#xrange(1,21))
 
         self.SetSizer(vz)
         self.Layout()
@@ -251,10 +285,13 @@ class Example(wx.Frame):
         self.Show(True)
 
     def reset_links(self,evt):
-        nl = range(1, 50)#int(random()*30)+1)
+        nl = 500# range(1, 50)#int(random()*30)+1)
         #nl=[1,]
         self.np.set_list(nl)
         print "reset done"
+        for kid in self.np.GetChildren():
+            print kid
+            #    fitmap = kid.GetWindow() #is the fitmap within
 
     def paged(self,evt):
         print "Got:", evt.page
